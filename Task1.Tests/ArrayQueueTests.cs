0571a55a0602a2036a31b5969825cf93ea268e67
AlexanderDemidovichV <alex.dem.v@gmail.com>
2017-04-19 22:27:49 +0300

    Adding tests

--- Task1.Tests/ArrayQueueTests.cs
+++ Task1.Tests/ArrayQueueTests.cs
@@ -10,26 +10,12 @@ namespace Task1.Tests
     [TestFixture]
     public class ArrayQueueTests
     {
-        [Test]
-        public void QueueDequeue()
-        {
-            ArrayQueue<string> queue = new ArrayQueue<string>(10);
-            queue.Enqueue("HybridTheory");
-            queue.Enqueue("Meteora");
-            queue.Enqueue("MinutesToMidnight");
-            queue.Enqueue("AThousandSuns");
-            queue.Enqueue("HybridTheory");
-            queue.Enqueue("LivingThings");
-            queue.Enqueue("TheHuntingParty");
-            Assert.AreEqual("HybridTheory", queue.Dequeue());
-            Assert.AreEqual("Meteora", queue.Dequeue());
-            Assert.AreEqual("MinutesToMidnight", queue.Dequeue());
-        }
+        private ArrayQueue<string> queue;
 
-        [Test]
-        public void QueueContains()
+        [SetUp]
+        protected void SetUp()
         {
-            ArrayQueue<string> queue = new ArrayQueue<string>(10);
+            queue = new ArrayQueue<string>(10);
             queue.Enqueue("HybridTheory");
             queue.Enqueue("Meteora");
             queue.Enqueue("MinutesToMidnight");
@@ -37,21 +23,11 @@ namespace Task1.Tests
             queue.Enqueue("HybridTheory");
             queue.Enqueue("LivingThings");
             queue.Enqueue("TheHuntingParty");
-            Assert.IsTrue(queue.Contains("AThousandSuns"));
-            Assert.IsTrue(queue.Contains("TheHuntingParty"));
         }
 
         [Test]
         public void QueueEnumerator()
         {
-            ArrayQueue<string> queue = new ArrayQueue<string>(10);
-            queue.Enqueue("HybridTheory");
-            queue.Enqueue("Meteora");
-            queue.Enqueue("MinutesToMidnight");
-            queue.Enqueue("AThousandSuns");
-            queue.Enqueue("HybridTheory");
-            queue.Enqueue("LivingThings");
-            queue.Enqueue("TheHuntingParty");
             using (var enumerator = queue.GetEnumerator())
             {
                 enumerator.MoveNext();
@@ -60,5 +36,28 @@ namespace Task1.Tests
                 Assert.AreEqual("Meteora", enumerator.Current);
             }
         }
+
+        [Test]
+        public void QueueDequeue()
+        {
+            Assert.AreEqual("HybridTheory", queue.Dequeue());
+            Assert.AreEqual("Meteora", queue.Dequeue());
+            Assert.AreEqual("MinutesToMidnight", queue.Dequeue());
+        }
+
+        [Test]
+        public void QueueContains()
+        {
+            Assert.IsTrue(queue.Contains("AThousandSuns"));
+            Assert.IsTrue(queue.Contains("TheHuntingParty"));
+        }
+
+        [Test]
+        public void QueuePeek()
+        {
+            Assert.IsTrue(queue.Contains("AThousandSuns"));
+            Assert.IsTrue(queue.Contains("TheHuntingParty"));
+        }
+
     }
 }
